669d0b57db0de42f487761e809d5c248227faa4e
pratham965 <146133782+pratham965@users.noreply.github.com>
2025-03-22 20:56:11 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,16 +1,22 @@
-import fastapi
-import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 
-app = fastapi.FastAPI(title="TransactAI")
+app = FastAPI(title="TransactAI")
 
 app.add_middleware(
-    fastapi.CORSMiddleware,
+    CORSMiddleware, 
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get('/')
+@app.get("/")
 def hello():
-    return {"server":"active"}
+    return {"server": "active"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.getenv("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
